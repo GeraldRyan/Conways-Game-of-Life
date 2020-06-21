c332f25c30f3911a71877f9f6b0c2fee5e8329e4
GeraldRyan <gerald.ryan40@yahoo.com>
2020-06-21 16:43:24 -0500

    found bug. got game working

--- gameoflife/src/App.js
+++ gameoflife/src/App.js
@@ -1,5 +1,4 @@
 import React, { useState, useCallback } from 'react';
-import logo from './logo.svg';
 import './App.css';
 import produce from 'immer'
 
@@ -19,54 +18,42 @@ const operations = [
 
 function App()
 {
-  const [grid, setGrid] = useState(() =>
-  {
+  const [grid, setGrid] = useState(() =>{
     const rows = []
-    for (let i = 0; i < numRows; i++)
-    {
+    for (let i = 0; i < numRows; i++){
       rows.push(Array.from(Array(numColumns), () => 0))
     }
     return rows
   })
 
-  const [running, setrunning] = useState(false)
+  const [running, setRunning] = useState(false)
 
   const runningRef = React.useRef(running)
   runningRef.current = running
 
-  const runSimulation = useCallback(() =>
-  {
-    if (!runningRef.current)
-    {
+  const runSimulation = useCallback(() =>{
+    if (!runningRef.current){
       return
     }
     // simulate
 
-    setGrid(g =>
-    {
-      return produce(g, gridCopy =>
-      {
-        for (let i = 0; i < numRows; i++)
-        {
-          for (let k = 0; k < numColumns; k++)
-          {
+    setGrid(g =>{
+      return produce(g, gridCopy =>{
+        for (let i = 0; i < numRows; i++){
+          for (let k = 0; k < numColumns; k++){
             let neighbors = 0
-            operations.forEach(([x, y]) =>
-            {
+            operations.forEach(([x, y]) =>{
               const newI = i + x
-              const newK = i + y
-              if (newI >= 0 && newI < numRows && newK >= 0 && newK < numColumns)
-              {
+              const newK = k + y
+              if (newI >= 0 && newI < numRows && newK >= 0 && newK < numColumns){
                 neighbors += g[newI][newK]
               }
             })
 
-            if (neighbors < 2 || neighbors > 3)
-            {
+            if (neighbors < 2 || neighbors > 3){
               gridCopy[i][k] = 0
             }
-            else if (g[i][k] === 0 && neighbors === 3)
-            {
+            else if (g[i][k] === 0 && neighbors === 3){
               gridCopy[i][k] = 1
 
             }
@@ -84,7 +71,7 @@ function App()
     <div className="App">
       <button onClick={() =>
       {
-        setrunning(!running)
+        setRunning(!running)
         if (!running)
         {
           runningRef.current = true
@@ -109,7 +96,7 @@ function App()
               {
                 const newGrid = produce(grid, gridCopy =>
                 {
-                  gridCopy[i][k] = gridCopy[i][k] ? 0 : 1
+                  gridCopy[i][k] = grid[i][k] ? 0 : 1
                 })
                 setGrid(newGrid)
               }}
